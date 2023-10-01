686a4bd7b837173eb87864a1f4e44980657cbd06
Codecamp-Preview <ahmadramadanirpl@gmail.com>
2023-10-01 18:40:04 +0700

    first commit

---
+++ src/components/ContactForm.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import NavigationBar from "./NavigationBar";
+import axios from "axios";
+
+const ContactForm = () => {
+  const [gateName, setName] = useState("");
+  const [gatePhone, setPhone] = useState("");
+  const [gateNote, setNote] = useState("");
+
+  const inputHandlerName = (name) => {
+    return setName(name);
+  };
+
+  const inputHandlerPhone = (phone) => {
+    return setPhone(phone);
+  };
+
+  const inputHandlerNote = (note) => {
+    return setNote(note);
+  };
+
+  function saveContact() {
+    axios({
+      method: "POST",
+      url: "https://api-codecamp.cyclic.cloud/api/contacts",
+      data: {
+        name: gateName,
+        phone: gatePhone,
+        note: gateNote,
+      },
+    }).then((resutls) => {
+      console.log(resutls);
+      if (resutls.data.payload.affectedRows) {
+        window.location.href = "/list-contact";
+      } else {
+        alert("Contact gagal ditambahkan ❎");
+      }
+    });
+  }
+
+  return (
+    <div>
+      <NavigationBar />
+      <div style={{ marginTop: 65 }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm m-10">
+              <label htmlFor="name" className="required">
+                Nama Lengkap
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                required="required"
+                onChange={(e) => inputHandlerName(e.target.value)}
+              />
+            </div>
+            <div className="col-sm m-10">
+              <label htmlFor="phone" className="required">
+                Nomor Telepon
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                onChange={(e) => inputHandlerPhone(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm m-10">
+              <label htmlFor="fullname">Catatan</label>
+              <textarea
+                type="text"
+                className="form-control"
+                onChange={(e) => inputHandlerNote(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm m-10">
+              <button
+                className="btn btn-danger"
+                type="button"
+                onClick={() => saveContact()}
+                style={{ cursor: "pointer" }}
+              >
+                Tambahkan Kontak
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
---
+++ src/components/ListContact.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import NavigationBar from "./NavigationBar";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const ListContact = () => {
+  const [getDatas, setDatas] = useState([]);
+
+  useEffect(() => {
+    console.log("get all datas from API contacts");
+    axios({
+      method: "GET",
+      url: "https://api-codecamp.cyclic.cloud/api/contacts",
+    }).then((resutls) => {
+      console.log(resutls);
+      setDatas(resutls.data.payload);
+    });
+  }, []);
+
+  function hapus(id) {
+    const isConfrim = window.confirm("Yakin ingin menghapus kontak ini?");
+
+    if (isConfrim) {
+      axios({
+        method: "DELETE",
+        url: `https://api-codecamp.cyclic.cloud/api/contacts/${id}`,
+      }).then((resutls) => {
+        if (resutls.data.payload.affectedRows) {
+          window.location.reload();
+        } else {
+          alert("gagal di hapus ❎");
+        }
+      });
+    }
+  }
+
+  return (
+    <div>
+      {console.log("get all data ", getDatas)}
+      <NavigationBar />
+      <div style={{ marginTop: 65 }}>
+        <div className="container-fluid">
+          <h3 className="text-center">Daftar Kontak Saya</h3>
+          <div className="w-full mw-full">
+            <div className="card p-0 bg-very-dark-dm">
+              <div className="table-responsive">
+                <table className="table table-inner-bordered">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Name Lengkap</th>
+                      <th>Nomor Telepon</th>
+                      <th>Catatan</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {getDatas.map((data, index) => {
+                      return (
+                        <tr key={index}>
+                          <th> {index + 1} </th>
+                          <td> {data.name} </td>
+                          <td> {data.phone} </td>
+                          <td>
+                            <div className="row">
+                              <div className="col-10"> {data.note} </div>
+                              <div className="col-1">
+                                <Link to={`/edit-contact/${data.id}`}>
+                                  <span
+                                    style={{ cursor: "pointer" }}
+                                    onClick={data.id}
+                                  >
+                                    🔍
+                                  </span>
+                                </Link>
+                              </div>
+                              <div className="col-1">
+                                <span
+                                  style={{ cursor: "pointer" }}
+                                  onClick={() => hapus(data.id)}
+                                >
+                                  ❌
+                                </span>
+                              </div>
+                            </div>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListContact;
